8b065955ca61cfa8ea2315f38f3ba0f477358259
machukhinktato <tarabrinmv@gmail.com>
2020-05-07 16:08:08 +0300

    template added

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask
 from config import Configuration
 
+
 app = Flask(__name__)
-
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-
-
-
+app.config.from_object(Configuration)
