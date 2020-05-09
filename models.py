f22e46e15fd2820def8b9eb600a01bbbf2f88536
machukhinktato <tarabrinmv@gmail.com>
2020-05-09 12:43:12 +0300

    methods initm generate_slug and repr defined

--- models.py
+++ models.py
@@ -1,5 +1,11 @@
 from app import db
 from datetime import datetime
+import re
+
+def slugify(s):
+    pattern = r'[^/w+]'
+    return re.sub(pattern, '-', s)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,3 +13,14 @@ class Post(db.Model):
     slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        self.slug = generate_slug()
+
+    def generate_slug(self):
+        if self.title:
+            self.slug = slugify(self.title)
+
+    def __repr__(self):
+        return f'<Post id: {self.id}, title: {self.title}>'
