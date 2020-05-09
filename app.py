6c081a93724b1dbdfa4f8e34e6257797ef330789
machukhinktato <tarabrinmv@gmail.com>
2020-05-09 11:57:51 +0300

    requirements added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import Configuration
 from posts.blueprint import posts, poster
 
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Configuration(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URL = 'psql+psycopg2://psql:geekbrains@localhost/blogonflask'
