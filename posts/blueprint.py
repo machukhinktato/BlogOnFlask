67dd2b29446b0c140ab8441ba1979ddd79001a99
machukhinktato <tarabrinmv@gmail.com>
2020-05-11 07:49:20 +0300

    blueprint view index edited to have possibilty to search

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,12 +1,21 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 from models import *
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(
+            Post.title.contains(q)
+            |
+            Post.body.contains(q)).all()
+    else:
+        posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
 @posts.route('/<slug>')
