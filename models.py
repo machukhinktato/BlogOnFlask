43fc02a6583989957a148431b76f8e5d8ff37d00
machukhinktato <tarabrinmv@gmail.com>
2020-05-14 01:44:37 +0300

    relationship between roles and users completed

--- models.py
+++ models.py
@@ -53,9 +53,15 @@ class Tag(db.Model):
         super(Tag, self).__init__(*args, **kwargs)
         self.slug = slugify(self.name)
 
-
     def __repr__(self):
         return f'<Tag id: {self.id}, name: {self.name}>'
+
+
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
 
 
 class User(db.Model, UserMixin):
@@ -63,7 +69,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
-
+    roles = db.relationship(
+        'Role',
+        secondary=roles_users,
+        backref=db.backref('users', lazy='dynamic')
+    )
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
