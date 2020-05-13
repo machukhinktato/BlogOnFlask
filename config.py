7e11a81081cf91432057d39d0b3ba7e197b5f1de
machukhinktato <tarabrinmv@gmail.com>
2020-05-13 23:02:21 +0300

    admin fixed by define secret_key in config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:geekbrains@localhost/blogonflask'
+    SECRET_KEY = 'something very secret'
