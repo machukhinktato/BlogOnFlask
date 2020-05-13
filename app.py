92ffc5b05511708ca8eab91713b5c822f5dfb5f1
machukhinktato <tarabrinmv@gmail.com>
2020-05-14 02:38:57 +0300

    flask security variables charged

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_security import SQLAlchemySessionUserDatastore
+from flask_security import Security
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -15,6 +17,11 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 from models import *
+
 admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
+
+user_datastore = SQLAlchemySessionUserDatastore(db, User, Role)
+
+security = Security(app, user_datastore)
