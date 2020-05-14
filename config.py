d6a52bfcd86b3af92fde0cfd5bc0057f301a0613
machukhinktato <tarabrinmv@gmail.com>
2020-05-14 15:25:20 +0300

    in config.py added variables for hash and salt

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:geekbrains@localhost/blogonflask'
     SECRET_KEY = 'something very secret'
+
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
