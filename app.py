18b02dfbbf4b1608cfaab17904203a15cdb3f55d
machukhinktato <tarabrinmv@gmail.com>
2020-05-14 15:00:20 +0300

    flask security imports and user_datastore variable fixed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_security import SQLAlchemySessionUserDatastore
+from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
 
 app = Flask(__name__)
@@ -22,6 +22,5 @@ admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
 
-user_datastore = SQLAlchemySessionUserDatastore(db, User, Role)
-
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
