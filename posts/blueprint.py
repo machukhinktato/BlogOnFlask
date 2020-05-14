6aba120b48b4c5ff3d7c47d756c528877214ac4a
machukhinktato <tarabrinmv@gmail.com>
2020-05-14 15:02:05 +0300

    login required decorators set

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -6,12 +6,15 @@ from flask import redirect
 from models import *
 from .forms import PostForm
 from app import db
+from flask_security import login_required
+
 posts = Blueprint('posts', __name__, template_folder='templates')
 
-@posts.route('/create', methods=['POST','GET'])
-def create_post():
 
-    if request.method=='POST':
+@posts.route('/create', methods=['POST', 'GET'])
+@login_required
+def create_post():
+    if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
 
@@ -22,15 +25,16 @@ def create_post():
         except:
             print('Something going wrong')
 
-        return redirect( url_for('posts.index'))
+        return redirect(url_for('posts.index'))
 
     form = PostForm()
     return render_template('posts/create_post.html', form=form)
 
 
 @posts.route('/<slug>/edit/', methods=['POST', 'GET'])
+@login_required
 def edit_post(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug == slug).first()
     if request.method == 'POST':
         form = PostForm(formdata=request.form, obj=post)
         form.populate_obj(post)
@@ -39,6 +43,7 @@ def edit_post(slug):
         return redirect(url_for('posts.post_detail', slug=post.slug))
     form = PostForm(obj=post)
     return render_template('posts/edit_post.html', post=post, form=form)
+
 
 @posts.route('/')
 def index():
@@ -56,23 +61,22 @@ def index():
             |
             Post.body.contains(q))
     else:
-        posts = Post.query.order_by(Post.created.desc())#.all()
+        posts = Post.query.order_by(Post.created.desc())  # .all()
 
         pages = posts.paginate(page=page, per_page=3)
 
-    return render_template('posts/index.html', posts=posts,  pages=pages)
-
+    return render_template('posts/index.html', posts=posts, pages=pages)
 
 
 @posts.route('/<slug>')
 def post_detail(slug):
-    post = Post.query.filter(Post.slug==slug).first()
+    post = Post.query.filter(Post.slug == slug).first()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
 
 
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
-    tag = Tag.query.filter(Tag.slug==slug).first()
+    tag = Tag.query.filter(Tag.slug == slug).first()
     posts = tag.posts.all()
-    return render_template('posts/tag_detail.html', tag = tag, posts = posts)
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
